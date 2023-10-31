6c787e13632e400e9084cf6dd5e723a7d8dd412f
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-31 13:40:25 +0000

    task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([a-zA-z0-9+]*)\] \[to:(.*?)\] \[flags:([0-9+-:]*)\]/).join
+puts ARGV[0].scan(/\[from:([a-zA-z0-9+]*)\] \[to:(.*?)\] \[flags:([0-9+-:]*)\]/).map { |match| match.join(',') }.join
