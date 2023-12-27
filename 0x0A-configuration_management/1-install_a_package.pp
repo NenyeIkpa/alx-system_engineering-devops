dd0db67591a069d7baf3a17c121e29b9bc17ced5
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-27 23:25:38 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Flask installation from pip3
 
-package { ['python-pip3']:
-  ensure => installed,
-}
-
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package['python-pip3']
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
