a65e910fac661a9093c24cf9857574838efb3579
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2024-02-16 08:16:55 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,17 +8,13 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of 10 hottest posts on the given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/limit=10.json".format(subreddit)
 
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/nenyeikpa_tech_jr)"
+v1.0.0 (by /u/nenyeikpa)"
     }
-    titles = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, titles=titles,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
         print("None")
